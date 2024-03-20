fe0a0b22d2a135e775f6b65bd515894746525c93
Sammy Ferreira <sammyfe2021@gmail.com>
2024-03-20 20:45:15 -0300

    feat: create event button

--- src/app/app/(main)/page.tsx
+++ src/app/app/(main)/page.tsx
@@ -1,10 +1,16 @@
 import DashboardPage from '@/components/dashboard/page'
+import { Button } from '@/components/ui/button'
+import { CiCirclePlus } from 'react-icons/ci'
 
 export default function Home() {
   return (
     <DashboardPage>
       <DashboardPage.Header>
         <DashboardPage.Header.Title>My Events</DashboardPage.Header.Title>
+        <Button className="h-fit p-0" variant={'link'}>
+          <CiCirclePlus className="mr-2 h-5 w-5" />
+          Create
+        </Button>
       </DashboardPage.Header>
       <DashboardPage.Main>oi</DashboardPage.Main>
     </DashboardPage>
