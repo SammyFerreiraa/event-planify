435bed904752b62a0e4508fa266cea7c6cc21ace
Sammy Ferreira <sammyfe2021@gmail.com>
2024-03-21 19:17:26 -0300

    refactor:  page components in Events and Settings

--- src/app/app/events/page.tsx
+++ src/app/app/events/page.tsx
@@ -1,5 +1,14 @@
 import DashboardPage from '@/components/dashboard/page'
 
 export default function Events() {
-  return <DashboardPage>Events</DashboardPage>
+  return (
+    <DashboardPage>
+      <DashboardPage.Header>
+        <DashboardPage.Header.Title>Events</DashboardPage.Header.Title>
+      </DashboardPage.Header>
+      <DashboardPage.Main>
+        <div>Events</div>
+      </DashboardPage.Main>
+    </DashboardPage>
+  )
 }
--- src/app/app/settings/page.tsx
+++ src/app/app/settings/page.tsx
@@ -1,5 +1,15 @@
 import DashboardPage from '@/components/dashboard/page'
+import { EventsTable } from './_components/events-table'
 
 export default function Settings() {
-  return <DashboardPage>oi</DashboardPage>
+  return (
+    <DashboardPage>
+      <DashboardPage.Header>
+        <DashboardPage.Header.Title>Settings</DashboardPage.Header.Title>
+      </DashboardPage.Header>
+      <DashboardPage.Main>
+        <EventsTable />
+      </DashboardPage.Main>
+    </DashboardPage>
+  )
 }
